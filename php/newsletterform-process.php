8354b945ac95a801c1d72de1ba7571942e22aa6a
Armando Víctor Pereira <armandinho14.ap@gmail.com>
2022-06-15 10:37:22 -0300

    Criar funcionalidade de enviar email

--- php/newsletterform-process.php
+++ php/newsletterform-process.php
@@ -1,9 +1,33 @@
 <?php
 $errorMSG = "";
 
-$tipo = $_POST["tipo"];
-$nome = $_POST["nome"];
+//Variáveis
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$tipo = $_POST['tipo'];
 $tel = $_POST["tel"];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
+
+//Compo E-mail
+$arquivo = "
+   <html>
+     <p><b>Nome: </b>$nome</p>
+     <p><b>E-mail: </b>$email</p>
+     <p><b>Celular: </b>$tel</p>
+     <p><b>Tipo: </b>$tipo</p>
+     <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+   </html>
+ ";
+
+//Emails para quem será enviado o formulário
+$destino = "foxsoftware21@gmail.com";
+$assunto = "Contato pelo Site";
+
+//Este sempre deverá existir para garantir a exibição correta dos caracteres
+$headers  = "MIME-Version: 1.0\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+$headers .= "From: $nome <$email>";
 
 if (empty($_POST["email"])) {
     $errorMSG = "Email is required ";
@@ -17,33 +41,16 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "foxsoftware21@gmail.com";
-$Subject = $tipo;
-
-// prepare email body text
-$Body = "";
-$Body .= "Nome: ";
-$Body .= $nome;
-$Body .= "Celular: ";
-$Body .= $tel;
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Terms: ";
-$Body .= $terms;
-$Body .= "\n";
-
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+$success = mail($destino, $assunto, $arquivo, $headers);
 
 // redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
+if ($success && $errorMSG == "") {
+    echo "success";
+} else {
+    if ($errorMSG == "") {
         echo "Algo deu errado :(";
     } else {
         echo $errorMSG;
     }
 }
-?>
